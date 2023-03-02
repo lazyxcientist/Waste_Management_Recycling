439493c3dc281f91c13ba7c8dac35be56f1d2a53
xcientist pankaj <105212276+lazyxcientist@users.noreply.github.com>
2023-03-03 04:04:22 +0530

    c

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('maps/',views.home,name='select_map'),\
+    path('',views.home,name='home'),
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('about',views.about,name='about'),
+    path('services',views.services,name='services'),
+    path('maps',views.maps,name='maps'),
+    path('rewards',views.rewards,name='rewards')
     
 ]
--- website/views.py
+++ website/views.py
@@ -2,5 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def maps(request):
+    
+    return render(request, 'maps.html')
+
 def home(request):
-    return render(request, 'maps.html', name='index')
+    context = {
+        'auth': request.user.is_authenticated
+    }
+    return render(request, 'index.html', context)
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def rewards(request):
+    return render(request, 'rewards.html')
